45d221b1a8d820ca2b372561eef6cef89d194ae7
sirius <95621941@qq.com>
2022-08-30 18:47:59 +0800

    Create gost.mk

---
+++ gost.mk
@@ -0,0 +1,8 @@
+wget https://github.com/ginuerzh/gost/releases/download/v2.11.2/gost-linux-amd64-2.11.2.gz
+
+gzip gost-linux-amd64-2.11.2.gz -d
+
+mv gost-linux-amd64-2.11.2 gost && chmod +x gost
+
+nohup ./gost -L=relay+tls://:9998 &
+
